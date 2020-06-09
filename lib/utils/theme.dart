b3bd03a21a2ff7c316cf822836a69c2804e69157
conclude91 <andri_conclude@yahoo.com>
2020-06-10 03:53:54 +0700

    - add splashscreen

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 final String appName = "UMkaMu";
 final Color backgroundColor = Color(0xFFFCFFFD);
 final Color primaryContentColor = Color(0xFF0c1821);
 final Color secondaryContentColor = Color(0xFFFCFFFD);
 final Color primaryColor = Color(0xFFFC5C65);
+final Color transparent = Colors.transparent;
 final String primaryFont = 'Roboto';
 final double smallSize = 17;
 final double mediumSize = 20;
 final double largeSize = 25;
+
+void setStatusBarColor(Color color) {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: color));
+}
